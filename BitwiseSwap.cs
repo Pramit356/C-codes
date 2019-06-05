47d412d7ab2f61b0f1dcf030d3bfb3f789a401d8
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-05 17:11:05 +0530

    Swap 2 numbers using bitwise XOR
    Something new learning here.

---
+++ BitwiseSwap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            Console.Write("Enter the two numbers (i and j): ");
+            string[] input = Console.ReadLine().Split();
+            int i = int.Parse(input[0]);
+            int j = int.Parse(input[1]);
+            Console.WriteLine("Before swapping, i = {0} and j = {1}", i, j);
+
+            i = i ^ j;
+            j = i ^ j;
+            i = i ^ j;
+            Console.WriteLine("After swapping, i = {0} and j = {1}", i, j);
+        }
+    }
+}
