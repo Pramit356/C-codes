f988d8edfe612b83a1c2f307b3fdc23468a0555e
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-05 17:47:02 +0530

    Display a diamond on console

---
+++ Diamond.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int number, i, k;
+            Console.Write("Enter number of rows: ");
+            number = int.Parse(Console.ReadLine());
+            int count = number - 1;
+            for (k = 1; k <= number; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= 2 * k - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            count = 1;
+            for (k = 1; k <= number - 1; k++)
+            {
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count++;
+                for (i = 1; i <= 2 * (number - k) - 1; i++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
