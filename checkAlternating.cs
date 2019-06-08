f13a0e1f63251fb897520a2a226595f4c5e8bad6
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-08 12:09:47 +0530

    Check whether the number is alternating
    First check if its binary ends with 0 or 1 using %2 operator. Then check the last digit and keep on shifting right and inspecting whether last digit has changed per shift. If no change then break and return false. Else return true.

---
+++ checkAlternating.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool checkAlternate(int num, int checkNum)
+        {
+            while (num != 0)
+            {
+                if (num % 2 == checkNum)
+                {
+                    num = num >> 1;
+                    checkNum = (checkNum == 1) ? 0 : 1;
+                }
+                else
+                    break;
+            }
+            if (num == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            bool check;
+
+            if (num % 2 == 0)
+            {
+                check = checkAlternate(num, 0);
+            }
+            else
+            {
+                check = checkAlternate(num, 1);
+            }
+            if (check == true)
+                Console.WriteLine("The number is alternating.");
+            else
+                Console.WriteLine("The number is not alternating.");
+        }
+    }
+}
