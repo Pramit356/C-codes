88941e37adbc0df059a49eeb25f93cf86c9369e8
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-09 13:22:28 +0530

    String to lower case.

---
+++ toLower.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+            StringBuilder equivalentStr = new StringBuilder(str);
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (equivalentStr[i] < (char)97)
+                {
+                    //int temp = equivalentStr[i];
+                    //temp += 32;
+                    //equivalentStr[i] = (char)temp;
+
+                    equivalentStr[i] = (char)((int)equivalentStr[i] + 32);
+                }
+            }
+            Console.WriteLine("All lower case: " + equivalentStr);
+            
+        }
+    }
+}
