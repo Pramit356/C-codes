ea5e170b03eb5f0e7c9029a394e5f187b85614e1
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-09 14:05:14 +0530

    Reverse words in a string

---
+++ reverseWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+            string[] eachword = str.Split();
+            string outputString="";
+            for(int i = eachword.Length-1; i >= 0; i--)
+            {
+                outputString += eachword[i] + " ";
+            }
+            Console.WriteLine(outputString);
+        }
+    }
+}
