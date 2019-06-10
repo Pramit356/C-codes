ebce82b17bea0a7bc106f15e04ea56eadb0f7473
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-10 19:21:13 +0530

    Length of a file in bytes

---
+++ fileLength.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            string path = @"C:\Users\Owner\source\repos\ConsoleApp1\ConsoleApp1\testFile.txt";
+            FileInfo finfo = new FileInfo(path);
+            Console.WriteLine("Length of the file is {0}", finfo.Length.ToString());
+        }
+    }
+}
