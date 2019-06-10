ae5cabfd6a34d0ced53982b780de9c9cf267e254
Pramit356 <33995641+Pramit356@users.noreply.github.com>
2019-06-10 20:10:30 +0530

    Maximum of three input numbers
    Uses ternary operators and string array converAll method.

---
+++ Number operations/MaxInThree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.Write("Enter the three numbers: ");
+            string[] stringArray = Console.ReadLine().Split();
+            int[] threenumbers = Array.ConvertAll(stringArray, s=>int.Parse(s));
+            int a, b, c;
+            a = threenumbers[0];
+            b = threenumbers[1];
+            c = threenumbers[2];
+
+            int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+
+            Console.WriteLine("Maximum number is " + max);
+        }
+    }
+}
